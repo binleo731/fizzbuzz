4984bac415849aae028d6a2e839f3d3a0c06060a
liaobin <liaobin@thoughtworks.com>
2020-01-10 09:50:58 +0800

    refactor: delete magic number

--- src/main/kotlin/com/thoughtworks/FizzBuzz.kt
+++ src/main/kotlin/com/thoughtworks/FizzBuzz.kt
@@ -1,30 +1,40 @@
 package com.thoughtworks
 
 class FizzBuzz {
+    companion object {
+        const val FIZZ_BUZZ_WHIZZ = "FizzBuzzWhizz"
+        const val FIZZ_BUZZ = "FizzBuzz"
+        const val FIZZ = "Fizz"
+        const val BUZZ = "Buzz"
+        const val WHIZZ = "Whizz"
+        const val MULTIPLE_OF_FIZZ = 3
+        const val MULTIPLE_OF_BUZZ = 5
+        const val MULTIPLE_OF_WHIZZ = 7
+    }
+
     fun runFizzBuzz(number: Int): String {
-
-        if (number % 3 == 0 && number % 5 == 0 && number % 7 == 0) {
-            return "FizzBuzzWhizz"
+        if (number % MULTIPLE_OF_FIZZ == 0 && number % MULTIPLE_OF_BUZZ == 0 && number % MULTIPLE_OF_WHIZZ == 0) {
+            return FIZZ_BUZZ_WHIZZ
         }
 
-        if (number % 3 == 0 && number % 5 == 0) {
-            return "FizzBuzz"
+        if (number % MULTIPLE_OF_FIZZ == 0 && number % MULTIPLE_OF_BUZZ == 0) {
+            return FIZZ_BUZZ
         }
 
-        if (number % 3 == 0) {
-            return "Fizz"
+        if (number % MULTIPLE_OF_FIZZ == 0) {
+            return FIZZ
         }
 
-        if (number % 5 == 0) {
-            return "Buzz"
+        if (number % MULTIPLE_OF_BUZZ == 0) {
+            return BUZZ
         }
 
-        if (number % 7 == 0) {
-            return "Whizz"
+        if (number % MULTIPLE_OF_WHIZZ == 0) {
+            return WHIZZ
         }
 
-        if (number.toString().contains("3")) {
-            return "Fizz"
+        if (number.toString().contains(MULTIPLE_OF_FIZZ.toString())) {
+            return FIZZ
         }
 
         return number.toString()
